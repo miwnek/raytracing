1e422ebdf636026bb88e94d1e35f7d07115e2312
miwnek <mwnek@student.agh.edu.pl>
2023-05-23 03:30:42 +0200

    Added tail recursion to the rayColor method

--- src/main/scala/base/Ray.scala
+++ src/main/scala/base/Ray.scala
@@ -9,7 +9,6 @@ class Ray (
 ) {
     def at(time: Double): Vector3D = origin + direction * time
 
-    // TODO: try to replace with tail recursion (* 0.5 -> acc= 0.5 ?)
     def rayColor(world: HittableList, depth: Int): Color = 
         if depth <= 0 then Color(0, 0, 0)
         else
@@ -21,11 +20,21 @@ class Ray (
                     clr
                 case HitRecord(point, frontFace, normal, t) => 
                     val target: Point3D = point + normal + Vector3D.randomInUnitSphere()
-                    Ray(point, target - point).rayColor(world, depth - 1) * 0.5
+                    val (clr: Color, pwr: Int) = Ray(point, target - point).rayColorTail(world, depth - 1, 1)
+                    clr * (math.pow(0.5, pwr))
+
+    @scala.annotation.tailrec
+    private final def rayColorTail(world: HittableList, depth: Int, power: Int): (Color, Int) = 
+        if depth <= 0 then (Color(0, 0, 0), power)
+        else
+            world hitBy(this, 0.001, infinity) match
+                case Missed => 
+                    val unitDirection: Vector3D = this.direction.unitVector
+                    val t: Double = 0.5 * (unitDirection.y + 1.0)
+                    val clr = Color(1.0, 1.0, 1.0) * (1.0 - t) + Color(0.5, 0.7, 1.0) * t
+                    (clr, power)
+                case HitRecord(point, frontFace, normal, t) => 
+                    val target: Point3D = point + normal + Vector3D.randomInUnitSphere()
+                    Ray(point, target - point).rayColorTail(world, depth - 1, power + 1)
 
 }
-
-
-// TODO: 
-// Ray -> trait
-// class RayActor extends Actor with Ray
